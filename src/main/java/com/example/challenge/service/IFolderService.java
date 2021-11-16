d509b7212b18c947a7189c25ad79f8d51923f7d2
emanuel-sceppaquercia <emanuelsce94@gmail.com>
2021-11-16 16:25:10 -0300

    Changes in services classes

--- src/main/java/com/example/challenge/service/IFolderService.java
+++ src/main/java/com/example/challenge/service/IFolderService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface IFolderService {
 
-    FolderDto CreateFolder(String name);
+    FolderDto createFolder(String name);
     Boolean deleteFolder(Long id);
     List<TaskDto> viewAllTasks(Long id);
 

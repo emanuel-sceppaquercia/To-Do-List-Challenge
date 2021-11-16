60087e8cdc6380f9e5e6054b505ebf48d1c3dc47
emanuel-sceppaquercia <emanuelsce94@gmail.com>
2021-11-16 10:19:48 -0300

    Folder and Task repository added

---
+++ src/main/java/com/example/challenge/repository/FolderRepository.java
@@ -0,0 +1,10 @@
+package com.example.challenge.repository;
+
+import com.example.challenge.model.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+}
---
+++ src/main/java/com/example/challenge/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.example.challenge.repository;
+
+import com.example.challenge.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}

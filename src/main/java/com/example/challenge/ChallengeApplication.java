69c212e9b48647df606732100c4a6a47f1f74f65
emanuel-sceppaquercia <emanuelsce94@gmail.com>
2021-11-16 16:25:52 -0300

    Small fix

--- src/main/java/com/example/challenge/ChallengeApplication.java
+++ src/main/java/com/example/challenge/ChallengeApplication.java
@@ -1,13 +1,20 @@
 package com.example.challenge;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }

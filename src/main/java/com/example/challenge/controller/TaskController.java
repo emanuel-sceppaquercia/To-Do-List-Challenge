251691d7c282713061d29a3df791f7a97722ad01
emanuel-sceppaquercia <emanuelsce94@gmail.com>
2021-11-17 22:13:53 -0300

    Small fix

--- src/main/java/com/example/challenge/controller/TaskController.java
+++ src/main/java/com/example/challenge/controller/TaskController.java
@@ -33,7 +33,7 @@ public class TaskController {
         return ResponseEntity.ok("Deleted");
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/check/{id}")
     public ResponseEntity<?> checkTask(@PathVariable(name = "id") Long id){
         taskService.checkTask(id);
         return ResponseEntity.ok("Done");
